5c3c0c79fbf45d188c2f5c47c5094ea140d777fe
Teejiinm <144406268+Teejiinm@users.noreply.github.com>
2024-12-13 18:31:59 +0900

    Update ApiEndPoint.js

--- Frontend/src/services/ApiEndPoint.js
+++ Frontend/src/services/ApiEndPoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://notezy-68s0.onrender.com',
     headers: {
         'Content-Type': 'application/json' 
     },
